125d361701b001b14f9e43822639bf53e69f4268
Valakas148 <ilyabychkov5x404@gmail.com>
2024-02-21 21:44:46 +0200

    HW1 complete

---
+++ homeworks/hw1/main.js
@@ -0,0 +1,55 @@
+
+// TASK 1
+let a1 = 'hello'
+let a2 = 'owu'
+let a3 = 'com'
+let a4 = 'ua'
+let a5 = 1
+let a6 = 10
+let a7 = -999
+let a8 = 123
+let a9 = 3.14
+let a10 = 2.7
+let a11 = 16
+let a12 = true
+let a13 = false
+
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a4)
+console.log(a5)
+console.log(a6)
+console.log(a7)
+console.log(a8)
+console.log(a9)
+console.log(a10)
+console.log(a11)
+console.log(a12)
+console.log(a13)
+
+// TASK 2
+
+let firstName = 'Bychkov'
+let middleName = 'Ilya'
+let lastName = 'Olegovych'
+
+console.log(`${firstName} ${middleName} ${lastName}`)
+console.log(firstName, middleName, lastName)
+
+// Task 3
+
+let a = 100; let b = '100'; let c = true;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+// Additional Task
+
+let name= prompt("Enter your name")
+let firstName_alert = prompt("Enter your firstname")
+let lastName_alert = prompt("Enter your lastname")
+alert("You are: " + `${name} ${firstName_alert} ${lastName_alert}`)
+console.log(name, firstName_alert, lastName_alert)
+
